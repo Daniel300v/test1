3a1b86f82dfe629bdffdbc407528467689027a12
Daniel hogg <daniel300v@gmail.co.uk>
2022-01-20 21:16:53 +0000

    almost done

--- src/main.rs
+++ src/main.rs
@@ -5,43 +5,61 @@ fn main() {
     //welcome message
     println!("welcome to noughts and crosses");
     //make defalt array and initial variables
-    let mut grid = [" "," "," "," "," "," "," "," "," ",];
+    let mut grid = [[" "," "," "],[" "," "," "],[" "," "," "]];
 
     let mut player = 0;
     let mut won = false;
     let mut x= -1;
     let mut y = -1;
+    let mut valid = false;
 
 
     //draw initial grid
     display(grid);
+
     while won == false{
-        //get row
-        let mut line = String::new();
-        println!("enter the row");
-        let y = std::io::stdin().read_line(&mut line).unwrap();
-        //get collumn
-        println!("enter the collumn");
-        let x = std::io::stdin().read_line(&mut line).unwrap();
+        while valid == false{
+            //get row
+            let mut line = String::new();
+            println!("enter the row");
+            let y = std::io::stdin().read_line(&mut line).unwrap();
+            //get collumn
+            println!("enter the collumn");
+            let x = std::io::stdin().read_line(&mut line).unwrap();
 
+            if grid[y][x] == " "{
+                valid = true;
+            }  
+
+        }
+        
+        if player == 1{
+            grid[y][x] == "X";
+        }
+        else{
+            grid[y][x] == "O";
+        }
+        player = switch_player(player)
     }
+    display(grid);
 }
 
 
-fn display(grid:[&str; 9]) {
-    println!("{}|{}|{}",grid[0],grid[1],grid[2]);
+fn display(grid:[[&str; 3]; 3]) {
+    println!("{}|{}|{}",grid[0][0],grid[0][1],grid[0][2]);
     println!("-----");
-    println!("{}|{}|{}",grid[3],grid[4],grid[5]);
+    println!("{}|{}|{}",grid[1][0],grid[1][1],grid[1][2]);
     println!("-----");
-    println!("{}|{}|{}",grid[6],grid[7],grid[8]);
+    println!("{}|{}|{}",grid[2][0],grid[2][1],grid[2][2]);
 }
 
 
-fn switchPlayer(player:i32) -> i32{
-    if (player == 0){
+fn switch_player(player:i32) -> i32{
+    if player == 0{
         return 1;
     }
     else{
         return 0;
     }
 }
+
