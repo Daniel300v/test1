3daad4e350e50ed0a89645705263283a8ec12034
Daniel hogg <daniel300v@gmail.co.uk>
2022-01-20 20:29:31 +0000

    main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,30 @@
-
+use std::io;
+use std::io::*;
 
 fn main() {
     //welcome message
     println!("welcome to noughts and crosses");
-    //make defalt array
+    //make defalt array and initial variables
     let mut grid = [" "," "," "," "," "," "," "," "," ",];
-    
+
+    let mut player = 0;
+    let mut won = false;
+    let mut x= -1;
+    let mut y = -1;
+
+
     //draw initial grid
-    display(grid)
+    display(grid);
+    while won == false{
+        //get row
+        let mut line = String::new();
+        println!("enter the row");
+        let y = std::io::stdin().read_line(&mut line).unwrap();
+        //get collumn
+        println!("enter the collumn");
+        let x = std::io::stdin().read_line(&mut line).unwrap();
+
+    }
 }
 
 
@@ -17,4 +34,14 @@ fn display(grid:[&str; 9]) {
     println!("{}|{}|{}",grid[3],grid[4],grid[5]);
     println!("-----");
     println!("{}|{}|{}",grid[6],grid[7],grid[8]);
+}
+
+
+fn switchPlayer(player:i32) -> i32{
+    if (player == 0){
+        return 1;
+    }
+    else{
+        return 0;
+    }
 }
